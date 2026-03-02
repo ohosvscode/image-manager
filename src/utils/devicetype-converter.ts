03f252fb888256d4f266a9305ce9580fc2bf2950
groupguanfang <1203970284@qq.com>
2026-03-02 12:07:28 +0800

    feat: add DeviceTypeConverter.camelcaseToSnakecase

--- src/utils/devicetype-converter.ts
+++ src/utils/devicetype-converter.ts
@@ -1,7 +1,7 @@
 import type { ListsFileItem, ProductConfigFile } from '../configs'
 
 export namespace DeviceTypeConverter {
-  export function snakecaseToCamelcase(snakecaseDeviceType: ListsFileItem.DeviceType | (string & {})): ProductConfigFile.DeviceType {
+  export function snakecaseToCamelcase(snakecaseDeviceType: ListsFileItem.DeviceTypeWithString): ProductConfigFile.DeviceType {
     switch (snakecaseDeviceType) {
       case 'phone':
         return 'Phone'
@@ -24,6 +24,32 @@ export namespace DeviceTypeConverter {
       // Fallback to lowercase, and replace underscores with spaces
       default:
         return snakecaseDeviceType.toLowerCase().split('_').join(' ') as ProductConfigFile.DeviceType
+    }
+  }
+
+  export function camelcaseToSnakecase(camelcaseDeviceType: ProductConfigFile.DeviceTypeWithString): ListsFileItem.DeviceType {
+    switch (camelcaseDeviceType) {
+      case 'Phone':
+        return 'phone'
+      case '2in1':
+        return '2in1'
+      case '2in1 Foldable':
+        return '2in1_foldable'
+      case 'Foldable':
+        return 'foldable'
+      case 'TV':
+        return 'tv'
+      case 'Tablet':
+        return 'tablet'
+      case 'TripleFold':
+        return 'triplefold'
+      case 'Wearable':
+        return 'wearable'
+      case 'WideFold':
+        return 'widefold'
+      // Fallback to lowercase, and replace spaces with underscores
+      default:
+        return camelcaseDeviceType.toLowerCase().split(' ').join('_') as ListsFileItem.DeviceType
     }
   }
 }
